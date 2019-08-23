e24d54039aa4dfc87310213cfdb09dcfe2876fa3
Maxim Zubarev <maxim1zubarev@gmail.com>
2019-08-23 19:18:28 +0300

    change single on observable

--- app/src/main/java/com/mldz/weatherforecat/utils/ApiEndpoint.kt
+++ app/src/main/java/com/mldz/weatherforecat/utils/ApiEndpoint.kt
@@ -1,7 +1,7 @@
 package com.mldz.weatherforecat.utils
 
 import com.mldz.weatherforecat.utils.model.Forecast
-import io.reactivex.Single
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,5 +11,5 @@ Created by Maxim Zubarev on 2019-08-23.
 
 interface ApiEndpoint {
     @GET("data/2.5/weather")
-    fun getWeatherNow(@Query("q") q: String): Single<Forecast>
+    fun getWeatherNow(@Query("q") q: String): Observable<Forecast>
 }
