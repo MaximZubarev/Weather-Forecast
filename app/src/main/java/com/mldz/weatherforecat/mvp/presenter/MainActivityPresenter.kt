ee1ae70e20ea36fd5b6e53b1219f5cceca692b62
Maxim Zubarev <maxim1zubarev@gmail.com>
2019-08-23 19:17:07 +0300

    add mvp classes for main activity

---
+++ app/src/main/java/com/mldz/weatherforecat/mvp/presenter/MainActivityPresenter.kt
@@ -0,0 +1,7 @@
+package com.mldz.weatherforecat.mvp.presenter
+
+/**
+Created by Maxim Zubarev on 2019-08-23.
+ */
+class MainActivityPresenter {
+}
---
+++ app/src/main/java/com/mldz/weatherforecat/mvp/view/MainActivityView.kt
@@ -0,0 +1,7 @@
+package com.mldz.weatherforecat.mvp.view
+
+/**
+Created by Maxim Zubarev on 2019-08-23.
+ */
+interface MainActivityView {
+}
