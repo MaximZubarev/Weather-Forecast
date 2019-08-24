c96dfdeb902b0fae764b3586e340dd374026cb2c
Maxim Zubarev <maxim1zubarev@gmail.com>
2019-08-24 17:32:53 +0300

    add common model for 2 types of forecast

--- app/src/main/java/com/mldz/weatherforecat/utils/model/FullForecast.kt
+++ app/src/main/java/com/mldz/weatherforecat/utils/model/FullForecast.kt
@@ -3,3 +3,7 @@ package com.mldz.weatherforecat.utils.model
 /**
 Created by Maxim Zubarev on 2019-08-24.
  */
+data class FullForecast(
+    val forecast: Forecast,
+    val forecastDays: ForecastDays
+)
