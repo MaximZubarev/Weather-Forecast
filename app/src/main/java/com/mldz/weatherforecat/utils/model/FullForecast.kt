1f458d10ebdda3a3efa18b82318ff2f559417ae7
Maxim Zubarev <maxim1zubarev@gmail.com>
2019-08-24 17:32:12 +0300

    change type

---
+++ app/src/main/java/com/mldz/weatherforecat/utils/model/FullForecast.kt
@@ -0,0 +1,5 @@
+package com.mldz.weatherforecat.utils.model
+
+/**
+Created by Maxim Zubarev on 2019-08-24.
+ */
--- app/src/main/java/com/mldz/weatherforecat/utils/model/Main.kt
+++ app/src/main/java/com/mldz/weatherforecat/utils/model/Main.kt
@@ -2,7 +2,7 @@ package com.mldz.weatherforecat.utils.model
 
 data class Main(
     val humidity: Int,
-    val pressure: Int,
+    val pressure: Double,
     val temp: Double,
     val temp_max: Double,
     val temp_min: Double
